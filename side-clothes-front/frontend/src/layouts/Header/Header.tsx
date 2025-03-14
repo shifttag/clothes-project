4e10e6fe5e4692d83a61e5dd85907e6871916d2a
shifttag <ehdgus0837@naver.com>
2025-03-14 17:10:34 +0900

    20250314

--- side-clothes-front/frontend/src/layouts/Header/Header.tsx
+++ side-clothes-front/frontend/src/layouts/Header/Header.tsx
@@ -3,31 +3,43 @@ import React, { useState } from "react";
 import * as css from "../../styles/MainStyle";
 import logo from "../../images/logo.png";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"; //
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Header() {
   const [isShopExpanded, setIsShopExpanded] = useState(false);
   const [isSupportExpanded, setIsSupportExpanded] = useState(false);
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
 
   const handleShopIconClick = () => {
     setIsShopExpanded(!isShopExpanded);
-    setIsSupportExpanded(false); // SUPPORT 상태 초기화
+    setIsSupportExpanded(false);
   };
 
   const handleSupportIconClick = () => {
     setIsSupportExpanded(!isSupportExpanded);
-    setIsShopExpanded(false); // SHOP 상태 초기화
+    setIsShopExpanded(false);
   };
 
   const handleSearch = () => {
-    alert("검색 기능은 준비 중입니다.");
-  }
+    setIsSearchExpanded(!isSearchExpanded);
+  };
+
+  const handleBackgroundClick = () => {
+    setIsSearchExpanded(false);
+  };
+
+  const handleCart = () => {
+    alert("장바구니 기능은 준비 중입니다.");
+  };
 
   return (
     <header>
       <div css={css.wrap}>
         <div css={css.webp}>
-          <img src={logo} alt="오류" />
+          <a href="/home">
+            <img src={logo} alt="오류" />
+          </a>
         </div>
         <div css={css.nav}>
           <div css={css.linkContainer}>
@@ -35,7 +47,9 @@ export default function Header() {
               SHOP
             </a>
             <button css={css.button} onClick={handleShopIconClick}>
-              <ChevronRightIcon css={[css.iconAnimation, isShopExpanded && css.rotatedIcon]} />
+              <ChevronRightIcon
+                css={[css.iconAnimation, isShopExpanded && css.rotatedIcon]}
+              />
             </button>
           </div>
           <div css={css.linkContainer}>
@@ -43,19 +57,113 @@ export default function Header() {
               SUPPORT
             </a>
             <button css={css.button} onClick={handleSupportIconClick}>
-              <ChevronRightIcon css={[css.iconAnimation, isSupportExpanded && css.rotatedIcon]} />
+              <ChevronRightIcon
+                css={[css.iconAnimation, isSupportExpanded && css.rotatedIcon]}
+              />
             </button>
           </div>
         </div>
         <div css={css.site_header_search}>
           <div>
-            <button css={css.button} onClick={handleSearch}>SEARCH</button>
+            <button css={css.button} onClick={handleSearch}>
+              SEARCH
+            </button>
           </div>
           <div>
-            <button css={css.button}>BAG</button>
+            <button css={css.button} onClick={handleCart}>
+              BAG
+            </button>
           </div>
         </div>
       </div>
+      {isShopExpanded && (
+        <div css={css.category}>
+          <div>
+            <a href="/" css={css.shoptag}>
+              SHOP ALL
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              OUTERWEAR
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              KNITWEAR
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              JACKET & VEST
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              T-SHIRT
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              SHIRT
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              PANTS
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              ACCESSORIES
+            </a>
+          </div>
+        </div>
+      )}
+      {isSupportExpanded && (
+        <div css={css.category}>
+          <div>
+            <a href="/" css={css.shoptag}>
+              CONTACT US
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              FAQ
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              SHIPPING
+            </a>
+          </div>
+          <div>
+            <a href="/" css={css.shoptag}>
+              RETURNS
+            </a>
+          </div>
+        </div>
+      )}
+      {isSearchExpanded && (
+        <div css={css.modalBackground} onClick={handleBackgroundClick}>
+          <div
+            css={css.searchModalContainer}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div css={css.searchModal}>
+              <SearchIcon />
+              <input
+                type="text"
+                placeholder="검색하기"
+                css={css.headerInputtag}
+              />
+              <div onClick={handleBackgroundClick}>
+                <CloseIcon css={css.closeIcon} />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
--- side-clothes-front/frontend/src/styles/MainStyle.ts
+++ side-clothes-front/frontend/src/styles/MainStyle.ts
@@ -1,24 +1,26 @@
-import { css, keyframes } from "@emotion/react";
+import { css } from "@emotion/react";
 
 export const wrap = css`
   display: flex;
   background-color: #fff;
   align-items: center;
   gap: 15px;
-`
+`;
 
 export const webp = css`
-  display: flex;
-  width: 100px;
-  height: 80px;
-`
+  & > a {
+    display: flex;
+    width: 100px;
+    height: 80px;
+  }
+`;
 
 export const atag = css`
   color: #000;
   text-decoration: none;
   font-size: 13.3333px;
-  font-family: 'Arial';
-`
+  font-family: "Arial";
+`;
 
 export const button = css`
   background-color: #fff;
@@ -26,8 +28,7 @@ export const button = css`
   border: none;
   padding: 0;
   cursor: pointer;
-
-`
+`;
 
 export const linkContainer = css`
   display: flex;
@@ -35,12 +36,12 @@ export const linkContainer = css`
   justify-content: center;
   gap: 8px;
   margin-right: 20px;
-`
+`;
 
 export const nav = css`
   display: flex;
   flex-shrink: 1;
-`
+`;
 
 export const site_header_search = css`
   display: flex;
@@ -48,7 +49,7 @@ export const site_header_search = css`
   justify-content: flex-end;
   gap: 40px;
   margin-right: 40px;
-`
+`;
 
 export const iconAnimation = css`
   transition: transform 0.1s ease-in-out;
@@ -57,3 +58,91 @@ export const iconAnimation = css`
 export const rotatedIcon = css`
   transform: rotate(90deg);
 `;
+
+export const shoptag = css`
+  color: #757575;
+  text-decoration: none;
+  font-size: 13.3333px;
+  font-family: "Arial";
+
+  &:hover {
+    color: #000;
+    text-decoration: underline;
+  }
+`;
+
+export const category = css`
+  display: flex;
+  gap: 40px;
+  margin-left: 115px;
+`;
+
+export const searchModalContainer = css`
+  position: fixed;
+  width: 100%;
+  height: 70px;
+  top: 80px;
+  background-color: #fff;
+`
+
+export const searchModal = css`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  margin-left: 110px;
+  margin-top: 10px;
+  gap: 10px;
+`
+
+export const modal = css`
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+`;
+
+export const modalOpen = css`
+  display: flex;
+  pointer-events: auto;
+`;
+
+export const modalBackground = css`
+  position: fixed;
+  top: 80px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #666666;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const headerInputtag = css`
+  border: none;
+  padding: 15px;
+  flex-grow: 1;
+  outline: none;
+`
+
+export const closeIcon = css`
+  display: none;
+  margin-right: 40px;
+  max-width: 1024px;
+  cursor: pointer;
+
+  :hover {
+    color: rgba(69, 69, 69, 0.5);
+  }
+  
+  @media (min-width: 1024px) {
+    display: block;
+  }
+`
+
